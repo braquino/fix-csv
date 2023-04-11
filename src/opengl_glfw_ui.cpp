1cdca4121f1c937555675156afeea7670d99557e
br_aquino <br_aquino@yahoo.com.br>
2023-04-11 15:16:16 +0200

    add imgui BaseUI wrapper to facade OpenGL and GLFW procedures

---
+++ src/opengl_glfw_ui.cpp
@@ -0,0 +1,97 @@
+#include "opengl_glfw_ui.h"
+#include "backend/imgui_impl_glfw.h"
+#include "backend/imgui_impl_opengl3.h"
+#include <stdio.h>
+#include <stdexcept>
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+OpenglGlfwUI::OpenglGlfwUI(int _width, int _height, std::string _title) : BaseUI(_width, _height, _title)
+{
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit())
+      throw std::runtime_error("Error trying set error callback to GLFW");
+
+  // GL 3.0 + GLSL 130
+  const char* glsl_version = "#version 130";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+
+  // Create window with graphics context
+  this->window = glfwCreateWindow(this->width, this->height, this->title.c_str(), NULL, NULL);
+  if (this->window == nullptr)
+      throw std::runtime_error("Error trying to initialize GLFW window");
+  glfwMakeContextCurrent(this->window);
+  glfwSwapInterval(1); // Enable vsync
+
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  this->io = &ImGui::GetIO(); (void)this->io;
+  this->io->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+  this->io->ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  //ImGui::StyleColorsLight();
+
+  // Setup Platform/Renderer backends
+  ImGui_ImplGlfw_InitForOpenGL(this->window, true);
+  ImGui_ImplOpenGL3_Init(glsl_version);
+}
+
+OpenglGlfwUI::~OpenglGlfwUI()
+{
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(this->window);
+  glfwTerminate();
+}
+
+void OpenglGlfwUI::start_loop(std::function<void()> func)
+{
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+  while (!glfwWindowShouldClose(this->window))
+  {
+      // Poll and handle events (inputs, window resize, etc.)
+      // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+      // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+      // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+      // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+      glfwPollEvents();
+
+      // Start the Dear ImGui frame
+      ImGui_ImplOpenGL3_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
+
+      // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+      func();
+
+      // Rendering
+      ImGui::Render();
+      int display_w, display_h;
+      glfwGetFramebufferSize(this->window, &display_w, &display_h);
+      glViewport(0, 0, display_w, display_h);
+      glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+      glClear(GL_COLOR_BUFFER_BIT);
+      ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+      glfwSwapBuffers(this->window);
+  }
+}
